c79cb2633eac1b7f45cd6fbec01818e5b843115d
IsmatKabir <Auni858@gmail.com>
2022-04-10 17:17:40 -0400

    first commit

--- src/demoA/DemoA1.java
+++ src/demoA/DemoA1.java
@@ -3,5 +3,6 @@ package demoA;
 public class DemoA1 {
  public static void main(String[] args) {
 	System.out.println("aunonto");
+	System.out.println("my name is aumi");
 }
 }
--- src/demoA/DemoB1.java
+++ src/demoA/DemoB1.java
@@ -3,6 +3,14 @@ package demoA;
 public class DemoB1 {
 public static void main(String[] args) {
 	System.out.println("aunonto");
+	System.out.println("aumi");
+	
+	
+	
+	
+	
+	
+	
 }
 	
 	
